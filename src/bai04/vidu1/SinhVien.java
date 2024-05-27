2709b3e8c151de95931f9cb22c583f70fc16b953
RonalTam <lakeheart001199@gmail.com>
2024-05-27 09:05:22 +0700

    ok

---
+++ src/bai04/vidu1/SinhVien.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bai04.vidu1;
+
+/**
+ *
+ * @author lakeh
+ */
+public class SinhVien extends Nguoi {
+    
+}
